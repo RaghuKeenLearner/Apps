7da9c39ce83ea16616778db3b8cce13503728c89
RaghuKeenLearner <60116750+RaghuKeenLearner@users.noreply.github.com>
2023-03-03 13:15:26 +1100

    Add files via upload

---
+++ Deploy-Application.ps1
@@ -0,0 +1,271 @@
+﻿<#
+.SYNOPSIS
+
+This script performs the setup on Windows 11 Start Menu
+
+
+https://psappdeploytoolkit.com
+#>
+
+
+[CmdletBinding()]
+Param (
+    [Parameter(Mandatory = $false)]
+    [ValidateSet('Install', 'Uninstall', 'Repair')]
+    [String]$DeploymentType = 'Install',
+    [Parameter(Mandatory = $false)]
+    [ValidateSet('Interactive', 'Silent', 'NonInteractive')]
+    [String]$DeployMode = 'Interactive',
+    [Parameter(Mandatory = $false)]
+    [switch]$AllowRebootPassThru = $false,
+    [Parameter(Mandatory = $false)]
+    [switch]$TerminalServerMode = $false,
+    [Parameter(Mandatory = $false)]
+    [switch]$DisableLogging = $false
+)
+
+Try {
+    ## Set the script execution policy for this process
+    Try {
+        Set-ExecutionPolicy -ExecutionPolicy 'ByPass' -Scope 'Process' -Force -ErrorAction 'Stop'
+    }
+    Catch {
+    }
+
+    ##*===============================================
+    ##* VARIABLE DECLARATION
+    ##*===============================================
+    ## Variables: Application
+    [String]$appVendor = ''
+    [String]$appName = ''
+    [String]$appVersion = ''
+    [String]$appArch = ''
+    [String]$appLang = 'EN'
+    [String]$appRevision = '01'
+    [String]$appScriptVersion = '1.0.0'
+    [String]$appScriptDate = '03/March/2023'
+    [String]$appScriptAuthor = 'Raghu Sharma'
+    ##*===============================================
+    ## Variables: Install Titles (Only set here to override defaults set by the toolkit)
+    [String]$installName = ''
+    [String]$installTitle = 'Windows 11 Start Menu'
+
+    ##* Do not modify section below
+    #region DoNotModify
+
+    ## Variables: Exit Code
+    [Int32]$mainExitCode = 0
+
+    ## Variables: Script
+    [String]$deployAppScriptFriendlyName = 'Deploy Application'
+    [Version]$deployAppScriptVersion = [Version]'3.9.2'
+    [String]$deployAppScriptDate = '02/02/2023'
+    [Hashtable]$deployAppScriptParameters = $PsBoundParameters
+
+    ## Variables: Environment
+    If (Test-Path -LiteralPath 'variable:HostInvocation') {
+        $InvocationInfo = $HostInvocation
+    }
+    Else {
+        $InvocationInfo = $MyInvocation
+    }
+    [String]$scriptDirectory = Split-Path -Path $InvocationInfo.MyCommand.Definition -Parent
+
+    ## Dot source the required App Deploy Toolkit Functions
+    Try {
+        [String]$moduleAppDeployToolkitMain = "$scriptDirectory\AppDeployToolkit\AppDeployToolkitMain.ps1"
+        If (-not (Test-Path -LiteralPath $moduleAppDeployToolkitMain -PathType 'Leaf')) {
+            Throw "Module does not exist at the specified location [$moduleAppDeployToolkitMain]."
+        }
+        If ($DisableLogging) {
+            . $moduleAppDeployToolkitMain -DisableLogging
+        }
+        Else {
+            . $moduleAppDeployToolkitMain
+        }
+    }
+    Catch {
+        If ($mainExitCode -eq 0) {
+            [Int32]$mainExitCode = 60008
+        }
+        Write-Error -Message "Module [$moduleAppDeployToolkitMain] failed to load: `n$($_.Exception.Message)`n `n$($_.InvocationInfo.PositionMessage)" -ErrorAction 'Continue'
+        ## Exit the script, returning the exit code to SCCM
+        If (Test-Path -LiteralPath 'variable:HostInvocation') {
+            $script:ExitCode = $mainExitCode; Exit
+        }
+        Else {
+            Exit $mainExitCode
+        }
+    }
+
+    #endregion
+    ##* Do not modify section above
+    ##*===============================================
+    ##* END VARIABLE DECLARATION
+    ##*===============================================
+
+    If ($deploymentType -ine 'Uninstall' -and $deploymentType -ine 'Repair') {
+        ##*===============================================
+        ##* PRE-INSTALLATION
+        ##*===============================================
+        [String]$installPhase = 'Pre-Installation'
+
+        ## Show Welcome Message
+        Show-InstallationWelcome
+
+        ## Show Progress Message (with the default message)
+        Show-InstallationProgress -StatusMessage "Setting up Windows 11 Start Menu."
+
+        ## <Perform Pre-Installation tasks here>
+        New-Item -ItemType Directory -Path C:\ProgramData\AutoPilotConfig\Start-Menu -Force -ErrorAction SilentlyContinue
+
+        ##*===============================================
+        ##* INSTALLATION
+        ##*===============================================
+        [String]$installPhase = 'Installation'
+
+        ## Handle Zero-Config MSI Installations
+        If ($useDefaultMsi) {
+            [Hashtable]$ExecuteDefaultMSISplat = @{ Action = 'Install'; Path = $defaultMsiFile }; If ($defaultMstFile) {
+                $ExecuteDefaultMSISplat.Add('Transform', $defaultMstFile)
+            }
+            Execute-MSI @ExecuteDefaultMSISplat; If ($defaultMspFiles) {
+                $defaultMspFiles | ForEach-Object { Execute-MSI -Action 'Patch' -Path $_ }
+            }
+        }
+
+        ## <Perform Installation tasks here>
+        Copy-Item -Path "$dirFiles\start2.bin" -Destination C:\ProgramData\AutoPilotConfig\Start-Menu -ErrorAction SilentlyContinue
+
+        ##*===============================================
+        ##* POST-INSTALLATION
+        ##*===============================================
+        [String]$installPhase = 'Post-Installation'
+
+        ## <Perform Post-Installation tasks here>
+        $BuildNumber=[INT](Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' | Select-Object -ExpandProperty CurrentBuildNumber)
+
+        If ($BuildNumber -lt 22621) {
+
+        New-Item -ItemType Directory -Path $env:SystemDrive\Users\Default\AppData\Local\Packages\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\LocalState -ErrorAction SilentlyContinue
+
+        $Source = "$env:SystemDrive\ProgramData\AutoPilotConfig\Start-Menu\start2.bin"
+
+        $Destination = "$env:SystemDrive\Users\*\AppData\Local\Packages\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\LocalState"
+
+        Get-ChildItem $Destination | ForEach-Object {Copy-Item -Path $Source -Destination $_ -Force}
+
+        Copy-Item -path $Source -Destination $env:SystemDrive\Users\Default\AppData\Local\Packages\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\LocalState
+
+        }
+
+        else {
+
+        New-Item -ItemType Directory -Path $env:SystemDrive\Users\Default\AppData\Local\Packages\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\LocalState
+
+        $Source = "$env:SystemDrive\ProgramData\AutoPilotConfig\Start-Menu\start2.bin"
+
+        $Destination = "$env:SystemDrive\Users\*\AppData\Local\Packages\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\LocalState"
+
+        Get-ChildItem $Destination | ForEach-Object {Copy-Item -Path $Source -Destination $_ -Force}
+
+        Copy-Item -path $Source -Destination $env:SystemDrive\Users\Default\AppData\Local\Packages\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\LocalState
+
+        }
+
+        ## Display a message at the end of the install
+        If (-not $useDefaultMsi) {
+            Show-InstallationPrompt -Message 'You can customize text to appear at the end of an install or remove it completely for unattended installations.' -ButtonRightText 'OK' -Icon Information -NoWait
+        }
+    }
+    ElseIf ($deploymentType -ieq 'Uninstall') {
+        ##*===============================================
+        ##* PRE-UNINSTALLATION
+        ##*===============================================
+        [String]$installPhase = 'Pre-Uninstallation'
+
+        ## Show Welcome Message, close Internet Explorer with a 60 second countdown before automatically closing
+        Show-InstallationWelcome -CloseApps 'iexplore' -CloseAppsCountdown 60
+
+        ## Show Progress Message (with the default message)
+        Show-InstallationProgress
+
+        ## <Perform Pre-Uninstallation tasks here>
+
+
+        ##*===============================================
+        ##* UNINSTALLATION
+        ##*===============================================
+        [String]$installPhase = 'Uninstallation'
+
+        ## Handle Zero-Config MSI Uninstallations
+        If ($useDefaultMsi) {
+            [Hashtable]$ExecuteDefaultMSISplat = @{ Action = 'Uninstall'; Path = $defaultMsiFile }; If ($defaultMstFile) {
+                $ExecuteDefaultMSISplat.Add('Transform', $defaultMstFile)
+            }
+            Execute-MSI @ExecuteDefaultMSISplat
+        }
+
+        ## <Perform Uninstallation tasks here>
+
+
+        ##*===============================================
+        ##* POST-UNINSTALLATION
+        ##*===============================================
+        [String]$installPhase = 'Post-Uninstallation'
+
+        ## <Perform Post-Uninstallation tasks here>
+
+
+    }
+    ElseIf ($deploymentType -ieq 'Repair') {
+        ##*===============================================
+        ##* PRE-REPAIR
+        ##*===============================================
+        [String]$installPhase = 'Pre-Repair'
+
+        ## Show Welcome Message, close Internet Explorer with a 60 second countdown before automatically closing
+        Show-InstallationWelcome -CloseApps 'iexplore' -CloseAppsCountdown 60
+
+        ## Show Progress Message (with the default message)
+        Show-InstallationProgress
+
+        ## <Perform Pre-Repair tasks here>
+
+        ##*===============================================
+        ##* REPAIR
+        ##*===============================================
+        [String]$installPhase = 'Repair'
+
+        ## Handle Zero-Config MSI Repairs
+        If ($useDefaultMsi) {
+            [Hashtable]$ExecuteDefaultMSISplat = @{ Action = 'Repair'; Path = $defaultMsiFile; }; If ($defaultMstFile) {
+                $ExecuteDefaultMSISplat.Add('Transform', $defaultMstFile)
+            }
+            Execute-MSI @ExecuteDefaultMSISplat
+        }
+        ## <Perform Repair tasks here>
+
+        ##*===============================================
+        ##* POST-REPAIR
+        ##*===============================================
+        [String]$installPhase = 'Post-Repair'
+
+        ## <Perform Post-Repair tasks here>
+
+
+    }
+    ##*===============================================
+    ##* END SCRIPT BODY
+    ##*===============================================
+
+    ## Call the Exit-Script function to perform final cleanup operations
+    Exit-Script -ExitCode $mainExitCode
+}
+Catch {
+    [Int32]$mainExitCode = 60001
+    [String]$mainErrorMessage = "$(Resolve-Error)"
+    Write-Log -Message $mainErrorMessage -Severity 3 -Source $deployAppScriptFriendlyName
+    Show-DialogBox -Text $mainErrorMessage -Icon 'Stop'
+    Exit-Script -ExitCode $mainExitCode
+}
